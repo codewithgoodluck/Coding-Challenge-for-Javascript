a45902862be03c87a68310a85d1d7d45447d2717
Goodluck <goodluckmordi4@gmail.com>
2021-11-05 07:38:58 +0100

    reverse a tring challnge

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("love")
+// Reverse a string challenge
+
+// Reverse the string hello
+
+// Fisrt Method
+function string(str){
+    var resuilt = str.split("").reverse("").join("")
+    console.log(resuilt)
+
+    
+}
+string("hello")
+
+
+// Second Method
+
+var resuilt = ""
+
+function string(str){
+
+    for(var i=str.length-1; i>=0; i--){
+        console.log(str[i])
+    }
+    
+    
+}
+string("hello")
