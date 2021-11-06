5a84794c82b2e600abd495758be0f8d8cf8af4c1
Goodluck <goodluckmordi4@gmail.com>
2021-11-06 10:30:40 +0100

    Another solution

--- script.js
+++ script.js
@@ -46,23 +46,45 @@
 
 // Check if a String is a Palindrome 
 
-function Palindrome(str){
-    // remove space, percent sign, underscore, pipe, colon, semicolon, etc are non-alphanumeric characters using reagex and replace methode
-    // var reg=/[\w_]/g;
-    // var smallStr= str.toLowerCase().replace(reg,"");
+// function Palindrome(str){
+//     // remove space, percent sign, underscore, pipe, colon, semicolon, etc are non-alphanumeric characters using reagex and replace methode
+//     // var reg=/[\w_]/g;
+//     // var smallStr= str.toLowerCase().replace(reg,"");
 
-    // reverse the string
+//     // reverse the string
 
-    // var string = '123abcABC-_*(!@#$%^&*()_-={}[]:\"<>,.?/~`';
-    var stripped = str.replace(/[^A-Za-z0-9]/g, '');
-    var revereString= stripped .split("").reverse("").join("");
-    console.log(revereString)
+//     // var string = '123abcABC-_*(!@#$%^&*()_-={}[]:\"<>,.?/~`';
+//     var stripped = str.replace(/[^A-Za-z0-9]/g, '');
+//     var revereString= stripped .split("").reverse("").join("");
 
-    if(revereString == str)return true;
-    return false;   
-}
 
-Palindrome("Eye$5^")
+//     if(str =! revereString){
+//         console.log("Not Palindrom")
+//     } else{
+//         console.log("Palindrom")
+//     }
+     
+// }
+
+// Palindrome("eye$ 5")
+
+
+
+function palindrome(str){
+    isPalindrome = true;
+    for(let i =0; i< str.length; i++){
+        console.log(str[i])
+      if(str[i]== str[(str.length - i)-1]){
+        isPalindrome = true;
+      }
+      else{
+        isPalindrome = false;
+      }
+    }
+    return isPalindrome;
+  }
+  console.log(palindrome("madam")); //true
+//   console.log(palindrome("foster")); // false
 
 
 
