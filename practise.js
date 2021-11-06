6514e6bbd7410886a0c85205c35a6fb1da4d3c93
Goodluck <goodluckmordi4@gmail.com>
2021-11-06 09:45:30 +0100

    Check for Palindrome

---
+++ practise.js
@@ -0,0 +1,53 @@
+// Reverse a string challenge
+
+// Reverse the string hello
+
+// fisrt method
+
+// function reverse(rev){
+
+// var final=rev.split("").reverse("").join("")
+
+// console.log(final)
+
+
+// }
+
+// reverse("Goodluck")
+
+// Second Method
+
+// function reverse(rev){
+
+//     var finalResuilt=""
+
+//     for(var i = rev.length-1; i>=0 ; i-- ){
+        
+//         finalResuilt+=rev[i]
+//     }
+
+//     console.log(finalResuilt)
+
+// }
+
+// reverse("Goodluck")
+
+
+// Second Challenge
+
+// Find the factorial of a number 10
+
+// function factoria(fac){
+
+//     var sum= 1;
+
+//     for(var i=0; i<=fac; i++ ){
+//          sum  +=sum * i
+//     }
+// console.log(sum)
+// }
+
+// factoria(4)
+
+
+
--- script.js
+++ script.js
@@ -2,26 +2,68 @@
 
 // Reverse the string hello
 
-// Fisrt Method
-function string(str){
-    var resuilt = str.split("").reverse("").join("")
-    console.log(resuilt)
+// // Fisrt Method
+// function string(str){
+//     var resuilt = str.split("").reverse("").join("")
+//     console.log(resuilt)
 
     
+// }
+// string("hello")
+
+
+// // Second Method
+
+// var resuilt = ""
+
+// function string(str){
+
+//     for(var i=str.length-1; i>=0; i--){
+//         console.log(str[i])
+//     }
+    
+    
+// }
+// string("hello")
+
+
+// Second Challenge
+
+// Find the factorial of a number 10
+
+// function number(num){
+//     var resuilt = 1;
+//     for(var i=0; i <= num; i++){
+//         resuilt += resuilt * i
+//     }
+    
+//     console.log(resuilt)
+    
+// }
+// number(4)
+
+// Task 3
+
+// Check if a String is a Palindrome 
+
+function Palindrome(str){
+    // remove space, percent sign, underscore, pipe, colon, semicolon, etc are non-alphanumeric characters using reagex and replace methode
+    // var reg=/[\w_]/g;
+    // var smallStr= str.toLowerCase().replace(reg,"");
+
+    // reverse the string
+
+    // var string = '123abcABC-_*(!@#$%^&*()_-={}[]:\"<>,.?/~`';
+    var stripped = str.replace(/[^A-Za-z0-9]/g, '');
+    var revereString= stripped .split("").reverse("").join("");
+    console.log(revereString)
+
+    if(revereString == str)return true;
+    return false;   
 }
-string("hello")
+
+Palindrome("Eye$5^")
 
 
-// Second Method
 
-var resuilt = ""
 
-function string(str){
-
-    for(var i=str.length-1; i>=0; i--){
-        console.log(str[i])
-    }
-    
-    
-}
-string("hello")
